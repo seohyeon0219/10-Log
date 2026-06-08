3cb5b40626c710780b2ae11bec5c2006c9962ff7
seohyeon0219 <seohyeonidaya@gmail.com>
2026-06-08 14:28:37 +0900

    refactor: outlet을 한 번만 렌더하도록 수정하고 반응형에 따라 바텀바 표시

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -1,20 +1,18 @@
 import { Outlet } from 'react-router-dom'
-import DesktopLayout from '../layouts/DesktopLayout'
-import MobileLayout from '../layouts/MobileLayout'
+import Header from '../components/desktop/Header'
+import Nav from '../components/desktop/Nav'
+import MobileBottomNavigation from '../components/navigation/MobileBottomNavigation'
 
 export default function AppPage() {
   return (
-    <>
+    <main className="mx-auto flex min-h-dvh w-full max-w-360 flex-col bg-stone-50 px-4 pt-5 pb-24 md:bg-white md:px-6 md:py-6">
       <div className="hidden md:block">
-        <DesktopLayout>
-          <Outlet />
-        </DesktopLayout>
+        <Header />
+        <Nav />
       </div>
-      <div className="block md:hidden">
-        <MobileLayout>
-          <Outlet />
-        </MobileLayout>
-      </div>
-    </>
+
+      <Outlet />
+      <MobileBottomNavigation />
+    </main>
   )
 }
